519d1a1dbb1b1905d6f8375565649fd1127d751d
tevans <thomas.evans@fiscal.treasury.gov>
2018-03-30 15:54:09 -0400

    Added a bootstrap template and did some work to lists

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { HttpClientModule, HTTP_INTERCEPTORS  } from '@angular/common/http';
-import { ReactiveFormsModule } from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
+import {ReactiveFormsModule} from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
-import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { LoginInterceptorService } from './login/login-interceptor.service';
-import { LoginService } from './login/login.service';
-import { ListsComponent } from './lists/lists.component';
-import { ListsService } from './lists/lists.service';
-import { ItemsComponent } from './items/items.component';
-import { ItemService } from './items/item.service';
+import {AppComponent} from './app.component';
+import {LoginComponent} from './login/login.component';
+import {LoginInterceptorService} from './login/login-interceptor.service';
+import {LoginService} from './login/login.service';
+import {ListsComponent} from './lists/lists.component';
+import {ListsService} from './lists/lists.service';
+import {ItemsComponent} from './items/items.component';
+import {ItemService} from './items/item.service';
 
 const appRoutes: Routes = [
-  { path: 'lists', component: ListsComponent },
-  { path: 'lists/:listId/items', component: ItemsComponent },
-  { path: 'login', component: LoginComponent },
-  { path: '**', redirectTo: '/lists', pathMatch: 'full' }
+  {path: 'lists', component: ListsComponent},
+  {path: 'lists/:listId/items', component: ItemsComponent},
+  {path: 'login', component: LoginComponent},
+  {path: '**', redirectTo: '/lists', pathMatch: 'full'}
 ];
 
 @NgModule({
@@ -47,4 +47,5 @@ const appRoutes: Routes = [
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/lists/lists.component.ts
+++ src/app/lists/lists.component.ts
@@ -1,6 +1,7 @@
-import { ListsService } from './../lists/lists.service';
-import { Component, OnInit } from '@angular/core';
-import { List } from '../shared/list';
+import {ListsService} from './lists.service';
+import {Component, OnInit} from '@angular/core';
+import {List} from '../shared/list';
+import {FormGroup, FormBuilder} from "@angular/forms";
 
 @Component({
   selector: 'app-lists',
@@ -8,15 +9,24 @@ import { List } from '../shared/list';
   styleUrls: ['./lists.component.css']
 })
 export class ListsComponent implements OnInit {
+  createListForm: FormGroup;
 
   lists: Array<List>;
 
-  constructor(private listsService: ListsService) { }
+  constructor(private listsService: ListsService, private formBuilder: FormBuilder) {
+  }
 
   ngOnInit() {
     this.listsService.getLists().subscribe((lists: Array<List>) => {
       this.lists = lists;
-    })
+    });
+    this.createListForm = this.formBuilder.group({
+      name: ['', []],
+    });
   }
-
+  commit() {
+    this.listsService.saveList(this.createListForm.value).subscribe(data => console.log(data));
+    console.log(this.createListForm.value);
+    this.createListForm.reset();
+  }
 }
