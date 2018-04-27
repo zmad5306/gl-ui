819e99602805ed6d2b98fbf8fdedc137c4c9c29a
tevans <thomas.evans@fiscal.treasury.gov>
2018-04-27 15:02:59 -0400

    added change detection stuff back

--- src/app/items/items.component.ts
+++ src/app/items/items.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {ItemService} from './item.service';
 import {Item} from '../shared/item';
@@ -11,7 +11,8 @@ import {FormBuilder, FormGroup} from '@angular/forms';
 @Component({
   selector: 'app-items',
   templateUrl: './items.component.html',
-  styleUrls: ['./items.component.css']
+  styleUrls: ['./items.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ItemsComponent implements OnInit {
 
@@ -26,7 +27,8 @@ export class ItemsComponent implements OnInit {
               private itemService: ItemService,
               private listsService: ListsService,
               private departmentService: DepartmentService,
-              private formBuilder: FormBuilder) {
+              private formBuilder: FormBuilder,
+              private _ref: ChangeDetectorRef) {
   }
 
   ngOnInit(): void {
@@ -34,13 +36,16 @@ export class ItemsComponent implements OnInit {
       this.listId = params['listId'];
       this.listsService.getList(this.listId).subscribe((list: List) => {
         this.list = list;
+        this._ref.markForCheck();
       });
       this.itemService.getItems(this.listId).subscribe((items: Array<Item>) => {
         this.items = items;
+        this._ref.markForCheck();
       });
     });
     this.departmentService.getDepartments().subscribe((departments: Array<Department>) => {
       this.departments = departments;
+      this._ref.markForCheck();
     });
     this.createItemForm = this.formBuilder.group({
       name: '',
@@ -67,6 +72,7 @@ export class ItemsComponent implements OnInit {
       () => {
         this.itemService.getItems(this.listId).subscribe((items: Array<Item>) => {
           this.items = items;
+          this._ref.markForCheck();
         });
       });
   }
@@ -77,6 +83,7 @@ export class ItemsComponent implements OnInit {
     this.itemService.saveItem(this.createItemForm.value).subscribe(data => {
       this.itemService.getItems(this.listId).subscribe((items: Array<Item>) => {
         this.items = items;
+        this._ref.markForCheck();
       });
     });
     this.createItemForm.reset();
